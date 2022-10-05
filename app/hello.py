a82807ecc582ed2afd7d05880b1075f229d5403f
Phillip Sitbon <phillip.sitbon@gmail.com>
2022-10-05 14:15:22 -0700

    print_hi(): don't allow empty names.

--- app/hello.py
+++ app/hello.py
@@ -3,4 +3,7 @@
 
 
 def print_hi(name1, name2):
+    if not name1 or not name2:
+        raise ValueError("name1 and name2 must not be empty")
+
     print(f"Hi, {name1} & {name2}!")
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,10 +3,16 @@ import pytest
 from app import hello
 
 
-@pytest.mark.parametrize("name1", ["pytest", "world", "you"])
-@pytest.mark.parametrize("name2", ["alice", "bob", "charlie"])
+@pytest.mark.parametrize("name1", ["pytest", "world", "you", ""])
+@pytest.mark.parametrize("name2", ["alice", "bob", "charlie", ""])
 def test_print_hi(capsys, name1, name2):
-    hello.print_hi(name1, name2)
-    captured = capsys.readouterr()
-    assert captured.out == f"Hi, {name1} & {name2}!\n"
-    assert captured.err == ""
+
+    if not name1 or not name2:
+        with pytest.raises(ValueError):
+            hello.print_hi(name1, name2)
+    else:
+        hello.print_hi(name1, name2)
+
+        captured = capsys.readouterr()
+        assert captured.out == f"Hi, {name1} & {name2}!\n"
+        assert captured.err == ""
